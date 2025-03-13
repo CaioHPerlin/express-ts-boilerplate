c826f7a13fe0e566ed7b1c66786df20b0e403bb6
CaioHPerlin <caiohperlin@gmail.com>
2025-03-13 16:35:17 -0400

    chore: init

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import env from 'dotenv';
+env.config();
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/ping', (_, res) => {
+  res.send('Pong!');
+});
+
+// Run Server
+app.listen(PORT, () =>
+  console.log(`[INFO] Server is running on http://localhost:${PORT}`),
+);
